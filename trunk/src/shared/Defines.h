e151f60744b44056ee8a2dc4a6740297fb460aa1
wayland <wayland@141d2eb5-4d50-5b48-94a8-3209c207636d>
2014-04-21 13:56:19 +0000

    + compile fix

--- trunk/src/shared/Defines.h
+++ trunk/src/shared/Defines.h
@@ -69,6 +69,7 @@
 	#include <sys/ioctl.h>
 	#include <sys/socket.h>
     #include <sys/times.h>
+    #include <sys/resource.h>
 	#include <netinet/in.h>
 	#include <arpa/inet.h>
 	#include <unistd.h>
