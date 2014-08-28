02b9378fd9f18c423b02c84bf5522e938034bd17
wayland <miroslav.kudrnac@hotmail.com>
2014-08-28 20:24:09 +0000

    + sql

--- src/console/Table.h
+++ src/console/Table.h
@@ -9,19 +9,45 @@
 #ifndef TransDB_Table_h
 #define TransDB_Table_h
 
-struct Collumn
+typedef enum E_SQL_DATA_TYPE
+{
+    esdtInt32       = 1,            //4 bytes
+    esdtInt64       = 2,            //8 bytes
+    esdtVarchar     = 3             //len coded string max 254 bytes - 1byte for len
+} E_SDT;
+
+typedef enum E_SQL_COLUMN_FLAGS
+{
+    escfNone            = 0,
+    escfAutoIncrement   = 1,        //autoincrement column - value is equal to ROWID
+    escfIndexed         = 2         //create index for this column
+    
+} E_SCF;
+
+typedef enum E_SQL_TABLE_FLAGS
+{
+    
+} E_STF;
+
+struct Column
 {
     uint8       m_dataType;         //1
-    uint8       m_position;         //1
+    uint8       m_dataSize;         //1 - used for store len of varchar column
     uint8       m_flags;            //1
-    char        m_name[5];          //5
+    char        m_name[8];          //8
+};
+
+struct Row
+{
+    uint64      m_rowId;            //8
+    void        *m_pData;           //8
 };
 
 struct TableDefinition
 {
-    uint8       m_collumns;         //1
+    uint8       m_columns;          //1
     uint8       m_flags;            //1
-    char        m_tableName[5];     //5
+    char        m_tableName[8];     //8
 };
 
 #endif
