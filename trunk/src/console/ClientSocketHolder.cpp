bd69c07d1c59fc73bcca7737fb3c52cd309a7e5c
wayland <wayland@141d2eb5-4d50-5b48-94a8-3209c207636d>
2014-09-22 15:29:50 +0000

    + stream send moved to void ClientSocketHolder::SendPacket(uint64 socketID, uint16 opcode, bbuff *pData)

--- trunk/src/console/ClientSocketHolder.cpp
+++ trunk/src/console/ClientSocketHolder.cpp
@@ -60,29 +60,65 @@ void ClientSocketHolder::SendPacket(uint64 socketID, const StackPacket &rPacket)
 
 void ClientSocketHolder::SendPacket(uint64 socketID, uint16 opcode, bbuff *pData)
 {
-    //stream send
+    //if data to send are bigger than sicket buffer split data to chunk and send chunk by chunk
     if(pData->size > g_cfg.SocketWriteBufferSize)
     {
-        //calc chunk size
-//        size_t chunkSize = g_cfg.SocketWriteBufferSize / 2;
-        
-        //send packet header
+        //stream send
+        std::lock_guard<std::recursive_mutex> rGuard(m_lock);
+        ClientSocketMap::iterator itr = m_clientSockets.find(socketID);
+        if(itr != m_clientSockets.end())
         {
-            std::lock_guard<std::recursive_mutex> rGuard(m_lock);
-            ClientSocketMap::iterator itr = m_clientSockets.find(socketID);
-            if(itr != m_clientSockets.end())
+            //create packet header
+            PackerHeader rHeader;
+            rHeader.m_opcode = opcode;
+            rHeader.m_size = (uint32)pData->size;
+            OUTPACKET_RESULT result = itr->second->SendRawData(&rHeader, sizeof(rHeader));
+            if(result != OUTPACKET_RESULT_SUCCESS)
             {
-                //create packet header
-                PackerHeader rHeader;
-                rHeader.m_opcode = opcode;
-                rHeader.m_size = (uint32)pData->size;
-                itr->second->SendRawData(&rHeader, sizeof(rHeader));
+                Log.Error(__FUNCTION__, "SendRawData failed. Opcode: %u, data size: %u, status: %u", opcode, (uint32)pData->size, (uint32)result);
+                return;
+            }
+            
+            //calc chunk size
+            size_t chunkSize = g_cfg.SocketWriteBufferSize / 2;
+            size_t rpos = 0;
+            uint32 sendCounter = 0;
+            //start sending chunks
+            while(rpos < pData->size)
+            {
+                size_t remainingData = pData->size - rpos;
+                size_t sendDataSize = std::min(remainingData, chunkSize);
+                //perform send
+            trySendAgain:
+                result = itr->second->SendRawData(pData->storage + rpos, sendDataSize);
+                if(result == OUTPACKET_RESULT_SUCCESS)
+                {
+                    //increment rpos
+                    rpos += sendDataSize;
+                    //send is ok continue with sending
+                    continue;
+                }
+                else if(result == OUTPACKET_RESULT_NO_ROOM_IN_BUFFER)
+                {
+                    if(sendCounter > 100)
+                    {
+                        Log.Error(__FUNCTION__, "SendRawData failed: no room in send buffer. Opcode: %u, data size: %u, status: %u", opcode, (uint32)pData->size, (uint32)result);
+                        break;
+                    }
+
+                    //socket buffer is full wait
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                    ++sendCounter;
+                    goto trySendAgain;
+                }
+                else
+                {
+                    //error interupt sending
+                    Log.Error(__FUNCTION__, "SendRawData failed. Opcode: %u, data size: %u, status: %u", opcode, (uint32)pData->size, (uint32)result);
+                    break;
+                }
             }
         }
-        
-        //start sending chunks
-        
-        
     }
     else
     {
