c2eaa60ee06ec30117adee6e99f8399b071ac0da
wayland <wayland@141d2eb5-4d50-5b48-94a8-3209c207636d>
2014-12-19 13:09:36 +0000

    + aligned malloc for *nix systems

--- trunk/src/console/BlockManager.c
+++ trunk/src/console/BlockManager.c
@@ -81,8 +81,15 @@ void blman_realloc_blocks(blman *self)
     self->blockCount += 1;
 
     //realloc block array
+#ifndef _ALIGNED_REALLOC
+    void *pNewBlocks = _ALIGNED_MALLOC(self->blockCount * BLOCK_SIZE, g_DataFileMallocAlignment);
+    memcpy(pNewBlocks, self->blocks, (self->blockCount - 1) * BLOCK_SIZE);
+    _ALIGNED_FREE(self->blocks);
+    assert(pNewBlocks);
+#else
     void *pNewBlocks = _ALIGNED_REALLOC(self->blocks, self->blockCount * BLOCK_SIZE, g_DataFileMallocAlignment);
     assert(pNewBlocks);
+#endif
     
     //set new pointer
     self->blocks = (uint8*)pNewBlocks;
